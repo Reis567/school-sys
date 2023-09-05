e8d0a153172976489908d841c503626533a0133c
Reis567 <matheusdosreis9@gmail.com>
2023-09-05 08:21:11 -0300

    initial header and home styling/html

--- school_sys/school_sys/urls.py
+++ school_sys/school_sys/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from core.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('', home, name='home'),
 ]
+# Adicione as configurações de URLs de mídia para desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
