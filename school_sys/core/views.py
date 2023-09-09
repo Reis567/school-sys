761acaf0cd773c71070474bae3ccb997239ddf02
Reis567 <matheusdosreis9@gmail.com>
2023-09-09 12:21:38 -0300

    register view

--- school_sys/core/views.py
+++ school_sys/core/views.py
@@ -1,6 +1,17 @@
-# views.py (no aplicativo "Core")
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def home(request):
     return render(request, 'core/home.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/register.html', {'form': form})
